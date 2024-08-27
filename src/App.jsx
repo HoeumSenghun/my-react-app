f4d6c1fb9d3f6f6398005718324fd0e999551d2d
Hoeum Senghun <hoeumsenghun369@gmail.com>
2024-08-28 01:21:39 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+  
+}
+
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>&copy; Your website name</p>
+        </footer>
+    );
+}
+
+
+export default Footer;
