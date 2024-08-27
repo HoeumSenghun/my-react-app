fc75b238c1a5d22007844681db12f45a702d1e07
Hoeum Senghun <hoeumsenghun369@gmail.com>
2024-08-28 02:21:14 +0700

    card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Food from "./Food.jsx"
+import Card from "./Card.jsx"
 
 
 
@@ -9,7 +10,10 @@ function App() {
   return (
     <>
       <Header />
+      <Card />
+      <Card />
       <Food />
+      <Card />
       <Footer />
     </>
   );
